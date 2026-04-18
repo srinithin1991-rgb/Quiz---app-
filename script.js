77d54490579527d0ec1e55b118ec21cd0edfe4f2
srinithin1991-rgb <srinithin1991@gmail.com>
2026-04-18 18:08:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
+function startQuiz() {
+  document.getElementById("quiz").innerHTML = `
+    <h3>Question 1: What is 2 + 2?</h3>
+    <button onclick="answer(true)">4</button>
+    <button onclick="answer(false)">5</button>
+  `;
+}
+
+function answer(correct) {
+  if (correct) {
+    alert("Correct!");
+  } else {
+    alert("Wrong!");
+  }
 }
